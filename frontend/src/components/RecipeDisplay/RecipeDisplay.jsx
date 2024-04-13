e4ad1c52e52170bdcf186dd855b9f4e69c6a32a3
justramle <91241669+justramle@users.noreply.github.com>
2024-04-13 02:30:16 -0400

    add menu pages

--- frontend/src/components/RecipeDisplay/RecipeDisplay.jsx
+++ frontend/src/components/RecipeDisplay/RecipeDisplay.jsx
@@ -6,6 +6,7 @@ import FoodItem from '../FoodItem/FoodItem'
 const RecipeDisplay = ({category, Title = "Trending Meals", titleStyle= {}}) => {
 
     const { food_list } = useContext(StoreContext)
+
   return (
     <div className='recipe-display' id='recipe-display'>
         <h2 style={titleStyle}> {Title} </h2>
@@ -13,10 +14,11 @@ const RecipeDisplay = ({category, Title = "Trending Meals", titleStyle= {}}) => 
 
             {food_list.map((item, index) => {
                 {console.log(category, item.category);}
-              //if category on all then display all else show only food match category
+              // if category on all then display all else show only food match category
                 if(category==="All" || category===item.category){
                   return <FoodItem key={index} id={item._id} name={item.name} description={item.description} time={item.time} image={item.image}/>
                 } 
+                
             })}
         </div>
     </div>
--- frontend/src/pages/Menu/Menu.jsx
+++ frontend/src/pages/Menu/Menu.jsx
@@ -2,8 +2,7 @@ import React, {useState} from 'react'
 import './Menu.css'
 import CategoryMenu from '../../components/CategoryMenu/CategoryMenu'
 import RecipeDisplay from '../../components/RecipeDisplay/RecipeDisplay'
-import FoodItem from '../../components/FoodItem/FoodItem'
-import { food_list } from '../../assets/assets';
+
 
 const Menu = () => {
     const [category, setCategory] = useState("All");
@@ -28,16 +27,6 @@ const Menu = () => {
      <CategoryMenu category={category} setCategory={setCategory} Title="The Latest Category" titleStyle={categoryStyle} />
       
       <RecipeDisplay category={category} Title="All Recipes" titleStyle={recipeStyle}/>
-      <div className="recipe-container">
-        {food_list.map(food => (
-          <FoodItem
-            name={food.name}
-            time={food.time}
-            description={food.description}
-            image={food.image}
-          />
-        ))}
-      </div>
 
     </div>
     
