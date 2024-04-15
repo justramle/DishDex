b0ee56820bd6a736f96dad6d27327880c0bf1ef3
michael-coulton <157097979+michael-coulton@users.noreply.github.com>
2024-04-15 19:31:54 -0400

    signup and dynamic navigation

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
          <div className="header-contents">
             <h2>Welcome <span className='orange-text' style={{ color: '#f5b70a' }}>D</span>ish<span className='orange-text' style={{ color: '#f5b70a' }}>D</span>ex</h2>
             <p>Lorem ipsum dolor, m quos dolorem maxime. Quas, quaerat nisi. Lorem ipsum dolor sit, amet consectetur adipisici.</p>
-            <button>View More</button>
+              <a href="/menu">
+                  <button id="signin">View More</button>
+              </a>
         </div>
         <div className="header-img">
             <img src="/logo.jpeg" alt="" className="headerImage" />
