29d5c678778520308ae3754f3effa32b8aec912e
justramle <91241669+justramle@users.noreply.github.com>
2024-04-15 13:52:46 -0400

    Change Profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 import About from './pages/About/About'
 import Menu from './pages/Menu/Menu'
-import Contact from './pages/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import Profile from './pages/Profile/Profile'
 
 const App = () => {
+    const location = useLocation();
+    const hideFooterRoute = ['/Login', '/Signup'];
+    const showFooter = !hideFooterRoute.includes(location.pathname);
+
     return (
         <div className='app'>
         {/* add components */}
@@ -20,8 +24,10 @@ const App = () => {
                 <Route path='/Signup' element={<Signup />} />
                 <Route path='/About' element={<About />} />
                 <Route path='/menu' element={<Menu />} />
-                <Route path='/menu' element={<Contact />} />
+                <Route path='/Profile' element={<Profile />} />
+                
         </Routes>
+        {showFooter && <Footer />}
         </div>
 
   )
