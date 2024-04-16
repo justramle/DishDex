8db10e8880da6e9867a89f01401117f3c83c2516
michael-coulton <157097979+michael-coulton@users.noreply.github.com>
2024-04-16 00:20:59 -0400

    added recipe page

---
+++ frontend/src/pages/Recipe/Recipe.jsx
@@ -0,0 +1,23 @@
+﻿import React, { useState } from 'react'
+import RecipeDetails from '../../components/RecipeDetails/RecipeDetails'
+import './Recipe.css'
+const Recipe = () => {
+    const [category, setCategory] = useState("menu");
+
+    /*
+    let input = {};
+    input["username"] = "";
+    input["email"] = "";
+    input["password"] = "";
+    input["confirm_password"] = "";
+    this.setState({ input: input });
+    */
+
+    return (
+        <div>
+            <RecipeDetails/>
+        </div>
+    )
+}
+
+export default Recipe
